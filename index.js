d7f00d54da7ed478dcce2932d36a86a36957aae1
jisoo <jsoo744@gmail.com>
2020-04-19 16:37:38 +0900

    [*] Static Type Checking

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Jisoo", age = 29, gender = "female";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and ${gender}.`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello")
+const name = "Jisoo",
+      age = 29,
+      gender = "female"
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age} and ${gender}.`)
+}
+
+sayHi(name, age)
+
+export {}
