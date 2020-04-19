9b6fd0e46583fcc935b4ff7d8d6473b43825566b
jisoo <jsoo744@gmail.com>
2020-04-19 17:22:21 +0900

    [*] Set tsc-watch start script

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Jisoo", age = 29, gender = "female";
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age} and ${gender}.`;
-};
-console.log(sayHi(name, age, gender));
-//# sourceMappingURL=index.js.map
