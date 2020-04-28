384d60b57ebde7d90c41495c3fd0ff5ce3bb58a1
jisoo <jsoo744@gmail.com>
2020-04-28 18:18:28 +0900

    [*] Add Hash and some getter methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateHash = (
+      index:number, 
+      previousHash: string, 
+      timestamp: number, 
+      data: string
+    ): string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString()  
 
   constructor(index: number,
     hash: string,
@@ -20,7 +30,11 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "20202020", "", "Hello", 123456)
 
-let blockchain: [Block] = [genesisBlock]
+let blockchain: Block[] = [genesisBlock]
 console.log(blockchain)
+
+const getBlockchain = (): Block[] => blockchain
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1]
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000)
 
 export {}
